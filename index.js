7568eaa91b5adaa3e16ab49d6f56687856478d34
Grace <grace_girly_melody@yahoo.com>
2018-08-22 19:38:48 +0700

    added day/night mechanism, changed greeting on homepage according to day/night

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 $(document).ready(()=>{
-	//$("body").addClass("body-inverted")
+	var currentDate = new Date();
+	var currentHour = currentDate.getHours();
+	console.log(currentDate);
+	if (currentHour >17 || currentHour<5){
+		$("body").addClass("body-inverted");
+		$("#celestialObject").attr("src","images/night.png");
+		$("#favicon").attr("href","images/favicon-night.png");
+	}
+	else{
+		$("#celestialObject").attr("src","images/cloudysun.png");
+		$("#favicon").attr("href","images/favicon-day.png");
+	}
+	
+	//greeting settings
+	if (currentHour > 5 && currentHour <= 10){
+		greeting="Good morning!";
+	}
+	else if (currentHour > 10 && currentHour <= 14){
+		greeting="Good afternoon!";
+	}
+	else if(currentHour > 14 && currentHour <= 20){
+		greeting="Good evening!";
+	}
+	else {
+		greeting="Good night!";
+	}
+
+	
+	
+	document.getElementById("greeting").innerHTML = greeting;
 })
